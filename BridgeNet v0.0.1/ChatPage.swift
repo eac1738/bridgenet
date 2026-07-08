d978d9b4e8c50934ef9eb3bb06911a948addecc1
MangoDragonHub <rashadpatt@gmail.com>
2026-07-08 10:48:50 -0500

    Updated ChatPage UI and added LocalAppDatabase

--- BridgeNet v0.0.1/ChatPage.swift
+++ BridgeNet v0.0.1/ChatPage.swift
@@ -9,9 +9,94 @@ import SwiftUI
 
 struct ChatPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle()
+                .ignoresSafeArea()
+                .foregroundStyle(Color.offwhite)
+            VStack()
+            {
+                HStack()
+                {
+                    Text("Ask BridgeNet")
+                        .font(.system(size: 30))
+                        .fontWeight(.semibold)
+                        .padding(.top, 30)
+                        .padding(.bottom, 3)
+                        .padding(.leading, 30)
+                    Spacer()
+                }
+                Divider()
+                //Chat Interactions
+                ZStack
+                {
+                    
+                    VStack
+                    {
+                        
+                        Rectangle()
+                            .ignoresSafeArea()
+                            .foregroundStyle(Color.gray)
+                    }
+                }
+                HStack
+                {
+                    ChatBar(userInput: "")
+                        .frame(width: 300, height: 3,alignment: .center)
+                    .padding(.bottom)
+                    .cornerRadius(1)
+                    .background(Color.white)
+                    HStack
+                    {
+                        Button("Send")
+                        {}
+                    }
+                }
+                    
+                
+            }
+         Spacer()
+        }
     }
 }
+
+struct ChatBar: View
+{
+    //This is supposed to hold the input area logic to talk to the chat bot.
+    @State var userInput : String
+    var body: some View
+    {
+        HStack()
+        {
+            TextField("Type Here", text: $userInput)
+                .onSubmit {
+                    sendInput(textInput: userInput)
+                }
+        }
+        
+        
+        
+    }
+}
+
+func sendInput(textInput: String)
+{
+    //Input Information that the user sends
+}
+
+struct ChatDialogBox : View
+{
+    //This is used to display what either the bot is talking or the user is talking
+    
+    var body: some View
+    {
+        Rectangle()
+            .frame(width: 250, height: 250)
+            .cornerRadius(30)
+        foregroundStyle(Color.green)
+    }
+}
+
+
 
 #Preview {
     ChatPage()
